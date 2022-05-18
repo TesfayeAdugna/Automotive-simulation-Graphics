139261288d745c1e7a02441a32ce750bb06db5ea
TesfayeAdugna <adugnatesfaye888@gmail.com>
2022-05-18 07:44:13 +0300

    rotating the 3d car

--- automotive.py
+++ automotive.py
@@ -6,9 +6,10 @@ from pygame.locals import *
 
 def init():
     pygame.init()
-    display = (800, 700)
-    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+    display = (500, 500)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
     gluOrtho2D(-20.0, 20.0, -20.0, 20.0)
+    
 
 vertexes = [(0,0,0),(-5,0,0),(-5,2,0),(0,2,0),(6.88,4,0),(1.88,4,0),(6.88,8,0),(1.88,8,0),(1.72,5,0),(-3.28,5,0)]
 edges = [(0,1),(1,2),(2,3),(0,3),(0,4),(4,5),(5,1),(4,6),(6,7),(7,5),(3,8),(8,9),(9,2),(9,7),(8,6)]
@@ -29,11 +30,17 @@ def draw():
 
 def main():
     init()
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+    gluPerspective(4, (display[1] / display[0]), 0.1, 50.0)
+    glTranslatef(0.0, 0.0, -30)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+        glRotatef(1,3,1,1)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         draw()
         pygame.display.flip()
         pygame.time.wait(10)
