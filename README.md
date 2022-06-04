d5ec0fe37eb00fcf5932732629cc599664f70ad3
TesfayeAdugna <adugnatesfaye888@gmail.com>
2022-06-05 02:00:29 +0300

    generating points for the Arches

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Automotive simulation using OpenGL python
+# Automotive simulation using OpenGL python
+This repository is for a Graphics course final project. The project is to develop an Automotive simulation using OpenGl.
+
+|   Team             | ID          |
+|:------------------:|:-----------:|
+| Tesfaye Adugna     |UGR/4709/12  |
+| Kenna Tefera       |UGR/1307/12  |
--- automotive.py
+++ automotive.py
@@ -1,56 +1,49 @@
+import random
 import pygame
 import numpy as np
+import points_info as p
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from pygame.locals import *
+
 
 def init():
     pygame.init()
     display = (500, 500)
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
-    gluOrtho2D(-20.0, 20.0, -20.0, 20.0)
+    gluOrtho2D(-30.0, 30.0, -30.0, 30.0)
     glClearColor(0.9, 0.9, 0.9, 0.0)
     
-vertexes = [(0,0,0),(-5,0,0),(-5,2,0),(0,2,0),(6.88,0,8),(1.88,0,8),(6.88,4,8),(1.88,4,8),(1.72,4,2),(-3.28,4,2)]
-edges = [(0,1),(1,2),(2,3),(0,3),(0,4),(4,5),(5,1),(4,6),(6,7),(7,5),(3,8),(8,9),(9,2),(9,7),(8,6)]
+
+def random_color():
+    x = random.randint(0, 255) / 255
+    y = random.randint(0, 255) / 255
+    z = random.randint(0, 255) / 255
+    color = (x, y, z)
+    return color
+
+colors_list= []
+
+for n in range(len(p.chair_faces_vector4)):
+    colors_list.append(random_color())
+
 
 def draw():
     glClear(GL_COLOR_BUFFER_BIT)
     
-    glLineWidth(2)
+    glBegin(GL_QUADS)
+    for face in p.chair_faces_vector4:
+        x = 0
+        for vertex in face:
+            x+=1
+            glColor3fv(p.colors[x%12])
+            glVertex3fv(p.chair_verticies_vector3[vertex])
+    glEnd()
+
     glBegin(GL_LINES)
-
-    
-
-    # glBegin(GL_POINTS)
-    r = 2
-    x = np.linspace(-0.2,0.2,1000)
-    z = np.linspace(-2,2,100)
-    y = np.sqrt(np.subtract(np.power(r,2),np.power(z,2)))
-    
-    glColor3f(0.0,0.0,0.0)
-    for i,j,k in zip(x,y,z):
-        # glVertex3fv((i+2.5-5,j,k+5.5))
-        # glVertex3fv((i+2.5-5,-j,k+5.5))
-
-        glVertex3fv((i+2.5-5,j,k))
-        glVertex3fv((i+2.5-5,-j,k))
-
-    glColor3f(1.0, 0.0, 1.0)
-    for edge in edges:
+    for edge in p.chair_edges_vector2:
         for i in edge:
-            glVertex3fv(vertexes[i])
-
-
-    # r = 2
-    # x = np.linspace(-2,2,100)
-    # y = np.sqrt(np.subtract(np.power(r,2),np.power(x,2)))
-    # z = np.linspace(0,0,1000)
-    # glColor3f(0.0,0.0,0.0)
-    # for i,j,k in zip(x,y,z):
-    #     glVertex3fv((i+2.5,j,k))
-    #     glVertex3fv((i+2.5,-j,k))
-
+            glVertex3fv(p.chair_verticies_vector3[i])
 
     glEnd()
     glFlush()
@@ -60,13 +53,14 @@ def main():
     display = (800, 600)
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
     gluPerspective(4, (display[1] / display[0]), 0.1, 50.0)
-    glTranslatef(0.0, 0.0, -30)
+    glTranslatef(0.0, 0.0, -20)
+    glRotatef(-90, 2, 0, 0)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-        glRotatef(1,0,5,1)
+        glRotatef(2, 3, -10, -45)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         draw()
         pygame.display.flip()
